4d779605ddf0c94b44b35fc4fe663966916e1fc0
kimsh322 <kimsh91322@gmail.com>
2023-04-12 16:06:30 +0900

    main

--- src/App.js
+++ src/App.js
@@ -6,12 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          여기는 <code>main branch</code> 입니다.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
+        <p>pull request 연습중</p>
       </header>
     </div>
   );
