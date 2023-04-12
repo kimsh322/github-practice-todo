4a643eb6f72d1f15aa4d482ec438b34fb4ac635c
kimsh322 <kimsh91322@gmail.com>
2023-04-12 16:29:42 +0900

    다시

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>pull request 연습중</p>
         <p>dev에서 작성한 내용</p>
-        <p>또 뭔가 적어서 합쳐봐야지</p>
       </header>
     </div>
   );
